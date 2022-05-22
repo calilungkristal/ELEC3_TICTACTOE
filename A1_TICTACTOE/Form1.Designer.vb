8405568a46262eaa04b17ac94903cf7c26594a5f
Kristal Calilung <klcalilung@iskolarngbayan.pup.edu.ph>
2022-05-22 11:46:12 +0800

    This commit is for the interface of the program.

---
+++ A1_TICTACTOE/Form1.Designer.vb
@@ -0,0 +1,300 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.lblTICTACTOE = New System.Windows.Forms.Label()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnNewGame = New System.Windows.Forms.Button()
+        Me.btnExitGame = New System.Windows.Forms.Button()
+        Me.lblPlayer0 = New System.Windows.Forms.Label()
+        Me.lblScorePlayer0 = New System.Windows.Forms.Label()
+        Me.lblPlayerX = New System.Windows.Forms.Label()
+        Me.lblScorePlayerX = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btn1
+        '
+        Me.btn1.BackColor = System.Drawing.Color.White
+        Me.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn1.Font = New System.Drawing.Font("Segoe UI", 39.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn1.Location = New System.Drawing.Point(12, 94)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(100, 100)
+        Me.btn1.TabIndex = 0
+        Me.btn1.UseVisualStyleBackColor = False
+        '
+        'btn2
+        '
+        Me.btn2.BackColor = System.Drawing.Color.White
+        Me.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn2.Font = New System.Drawing.Font("Segoe UI", 39.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn2.Location = New System.Drawing.Point(118, 94)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(100, 100)
+        Me.btn2.TabIndex = 1
+        Me.btn2.UseVisualStyleBackColor = False
+        '
+        'btn3
+        '
+        Me.btn3.BackColor = System.Drawing.Color.White
+        Me.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn3.Font = New System.Drawing.Font("Segoe UI", 39.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn3.Location = New System.Drawing.Point(224, 94)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(100, 100)
+        Me.btn3.TabIndex = 2
+        Me.btn3.UseVisualStyleBackColor = False
+        '
+        'btn6
+        '
+        Me.btn6.BackColor = System.Drawing.Color.White
+        Me.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn6.Font = New System.Drawing.Font("Segoe UI", 39.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn6.Location = New System.Drawing.Point(224, 200)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(100, 100)
+        Me.btn6.TabIndex = 5
+        Me.btn6.UseVisualStyleBackColor = False
+        '
+        'btn5
+        '
+        Me.btn5.BackColor = System.Drawing.Color.White
+        Me.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn5.Font = New System.Drawing.Font("Segoe UI", 39.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn5.Location = New System.Drawing.Point(118, 200)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(100, 100)
+        Me.btn5.TabIndex = 4
+        Me.btn5.UseVisualStyleBackColor = False
+        '
+        'btn4
+        '
+        Me.btn4.BackColor = System.Drawing.Color.White
+        Me.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn4.Font = New System.Drawing.Font("Segoe UI", 39.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn4.Location = New System.Drawing.Point(12, 200)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(100, 100)
+        Me.btn4.TabIndex = 3
+        Me.btn4.UseVisualStyleBackColor = False
+        '
+        'btn9
+        '
+        Me.btn9.BackColor = System.Drawing.Color.White
+        Me.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn9.Font = New System.Drawing.Font("Segoe UI", 39.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn9.Location = New System.Drawing.Point(224, 306)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(100, 100)
+        Me.btn9.TabIndex = 8
+        Me.btn9.UseVisualStyleBackColor = False
+        '
+        'btn8
+        '
+        Me.btn8.BackColor = System.Drawing.Color.White
+        Me.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn8.Font = New System.Drawing.Font("Segoe UI", 39.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn8.Location = New System.Drawing.Point(118, 306)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(100, 100)
+        Me.btn8.TabIndex = 7
+        Me.btn8.UseVisualStyleBackColor = False
+        '
+        'btn7
+        '
+        Me.btn7.BackColor = System.Drawing.Color.White
+        Me.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn7.Font = New System.Drawing.Font("Segoe UI", 39.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn7.Location = New System.Drawing.Point(12, 306)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(100, 100)
+        Me.btn7.TabIndex = 6
+        Me.btn7.UseVisualStyleBackColor = False
+        '
+        'lblTICTACTOE
+        '
+        Me.lblTICTACTOE.BackColor = System.Drawing.Color.Navy
+        Me.lblTICTACTOE.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.lblTICTACTOE.Font = New System.Drawing.Font("Arial Rounded MT Bold", 39.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblTICTACTOE.ForeColor = System.Drawing.Color.White
+        Me.lblTICTACTOE.Location = New System.Drawing.Point(12, 12)
+        Me.lblTICTACTOE.Margin = New System.Windows.Forms.Padding(3)
+        Me.lblTICTACTOE.Name = "lblTICTACTOE"
+        Me.lblTICTACTOE.Size = New System.Drawing.Size(624, 76)
+        Me.lblTICTACTOE.TabIndex = 9
+        Me.lblTICTACTOE.Text = "TIC TAC TOE"
+        Me.lblTICTACTOE.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnReset
+        '
+        Me.btnReset.AutoSize = True
+        Me.btnReset.BackColor = System.Drawing.Color.Navy
+        Me.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnReset.Font = New System.Drawing.Font("Arial Rounded MT Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnReset.ForeColor = System.Drawing.Color.White
+        Me.btnReset.Location = New System.Drawing.Point(330, 306)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(150, 45)
+        Me.btnReset.TabIndex = 10
+        Me.btnReset.Text = "RESET"
+        Me.btnReset.UseVisualStyleBackColor = False
+        '
+        'btnNewGame
+        '
+        Me.btnNewGame.AutoSize = True
+        Me.btnNewGame.BackColor = System.Drawing.Color.Navy
+        Me.btnNewGame.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnNewGame.Font = New System.Drawing.Font("Arial Rounded MT Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnNewGame.ForeColor = System.Drawing.Color.White
+        Me.btnNewGame.Location = New System.Drawing.Point(486, 306)
+        Me.btnNewGame.Name = "btnNewGame"
+        Me.btnNewGame.Size = New System.Drawing.Size(150, 45)
+        Me.btnNewGame.TabIndex = 11
+        Me.btnNewGame.Text = "NEW GAME"
+        Me.btnNewGame.UseVisualStyleBackColor = False
+        '
+        'btnExitGame
+        '
+        Me.btnExitGame.AutoSize = True
+        Me.btnExitGame.BackColor = System.Drawing.Color.Maroon
+        Me.btnExitGame.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnExitGame.Font = New System.Drawing.Font("Arial Rounded MT Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnExitGame.ForeColor = System.Drawing.Color.White
+        Me.btnExitGame.Location = New System.Drawing.Point(330, 361)
+        Me.btnExitGame.Name = "btnExitGame"
+        Me.btnExitGame.Size = New System.Drawing.Size(306, 45)
+        Me.btnExitGame.TabIndex = 12
+        Me.btnExitGame.Text = "EXIT GAME"
+        Me.btnExitGame.UseVisualStyleBackColor = False
+        '
+        'lblPlayer0
+        '
+        Me.lblPlayer0.BackColor = System.Drawing.Color.Orange
+        Me.lblPlayer0.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.lblPlayer0.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblPlayer0.Location = New System.Drawing.Point(330, 94)
+        Me.lblPlayer0.Name = "lblPlayer0"
+        Me.lblPlayer0.Size = New System.Drawing.Size(150, 100)
+        Me.lblPlayer0.TabIndex = 13
+        Me.lblPlayer0.Text = "PLAYER 0:"
+        Me.lblPlayer0.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblScorePlayer0
+        '
+        Me.lblScorePlayer0.BackColor = System.Drawing.Color.Brown
+        Me.lblScorePlayer0.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.lblScorePlayer0.Font = New System.Drawing.Font("Arial Rounded MT Bold", 35.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblScorePlayer0.ForeColor = System.Drawing.Color.White
+        Me.lblScorePlayer0.Location = New System.Drawing.Point(330, 200)
+        Me.lblScorePlayer0.Name = "lblScorePlayer0"
+        Me.lblScorePlayer0.Size = New System.Drawing.Size(150, 100)
+        Me.lblScorePlayer0.TabIndex = 14
+        Me.lblScorePlayer0.Text = "0"
+        Me.lblScorePlayer0.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblPlayerX
+        '
+        Me.lblPlayerX.BackColor = System.Drawing.Color.Orange
+        Me.lblPlayerX.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.lblPlayerX.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblPlayerX.Location = New System.Drawing.Point(486, 94)
+        Me.lblPlayerX.Name = "lblPlayerX"
+        Me.lblPlayerX.Size = New System.Drawing.Size(150, 100)
+        Me.lblPlayerX.TabIndex = 15
+        Me.lblPlayerX.Text = "PLAYER X:"
+        Me.lblPlayerX.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblScorePlayerX
+        '
+        Me.lblScorePlayerX.BackColor = System.Drawing.Color.Brown
+        Me.lblScorePlayerX.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.lblScorePlayerX.Font = New System.Drawing.Font("Arial Rounded MT Bold", 35.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblScorePlayerX.ForeColor = System.Drawing.Color.White
+        Me.lblScorePlayerX.Location = New System.Drawing.Point(486, 200)
+        Me.lblScorePlayerX.Name = "lblScorePlayerX"
+        Me.lblScorePlayerX.Size = New System.Drawing.Size(150, 100)
+        Me.lblScorePlayerX.TabIndex = 16
+        Me.lblScorePlayerX.Text = "0"
+        Me.lblScorePlayerX.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BackColor = System.Drawing.Color.LightSteelBlue
+        Me.ClientSize = New System.Drawing.Size(649, 419)
+        Me.Controls.Add(Me.lblScorePlayerX)
+        Me.Controls.Add(Me.lblPlayerX)
+        Me.Controls.Add(Me.lblScorePlayer0)
+        Me.Controls.Add(Me.lblPlayer0)
+        Me.Controls.Add(Me.btnExitGame)
+        Me.Controls.Add(Me.btnNewGame)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.lblTICTACTOE)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "TIC TAC TOE"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents lblTICTACTOE As Label
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnNewGame As Button
+    Friend WithEvents btnExitGame As Button
+    Friend WithEvents lblPlayer0 As Label
+    Friend WithEvents lblScorePlayer0 As Label
+    Friend WithEvents lblPlayerX As Label
+    Friend WithEvents lblScorePlayerX As Label
+End Class
